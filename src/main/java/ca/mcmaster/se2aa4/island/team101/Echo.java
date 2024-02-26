2086290fb34bc7e7de610792cfdd38cd211d4997
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-02-25 20:44:59 -0500

    added commands class (close #77)

--- src/main/java/ca/mcmaster/se2aa4/island/team101/Echo.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.se2aa4.island.team101;
-
-public class Echo {
-    protected static final String ECHO = "echo";
-
-    public Echo(){}
-}
--- src/main/java/ca/mcmaster/se2aa4/island/team101/Scan.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.se2aa4.island.team101;
-
-public class Scan {
-    protected static final String SCAN = "scan";
-
-    public Scan(){}
-}
