9f56cc563faef1676b3f2be597590a9a1ce5e4ae
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-02-28 13:57:21 -0500

    refactor - better to have direction NESW strings in Decision, not AirDecision; all decisions need it

--- src/main/java/ca/mcmaster/se2aa4/island/team101/Decision.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team101/Decision.java
@@ -3,14 +3,15 @@ package ca.mcmaster.se2aa4.island.team101;
 public abstract class Decision {
     protected Traveler traveler;
     protected EmergencyDetector emergency;
+    
+        // Don't wanna hard code the strings in for parameter passing
+        protected static final String NORTH = "N";
+        protected static final String EAST = "E";
+        protected static final String SOUTH = "S";
+        protected static final String WEST = "W";
 
     // so that the command strings can be easily changed if the string to do whichever action
     // changes, might need to add more not sure all the available commands
-    protected static final String FLY = "fly";
-    protected static final String STOP = "stop";
-    protected static final String HEADING = "heading";
-    protected static final String ECHO = "echo";
-    protected static final String SCAN = "scan";
 
     public Decision(Traveler traveler) {
         this.traveler = traveler;
