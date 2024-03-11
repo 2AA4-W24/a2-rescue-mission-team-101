27dfadb121ce6ebcd8bf71bc73d8ed20b530d548
git@github.com <savvy.liu@hotmail.com>
2024-03-10 20:07:57 -0400

    Updated drone so that previous commands will perform the corresponding things for that command

--- src/main/java/ca/mcmaster/se2aa4/island/team101/AirDecision.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team101/AirDecision.java
@@ -11,7 +11,6 @@ public class AirDecision extends Decision {
     Response response; // will be initialized accordidng to type -> using polymorphism
 
     private int counter = 0;
-    private String lastCommand; 
     // use getters from drone to get other relevant info/objects for decision logic
     // should only need emergency detector and battery through drone
     // commands cover everything else i think
@@ -22,26 +21,16 @@ public class AirDecision extends Decision {
 
     @Override
     public String decide() {
-        if (counter > 20){
-            // just for the mvp, it checks for land and returns home immediately
-            // ideally, this is put into a method, but since its just temporary, it'll just be done in the if statement
-            // need to implement a drone.goHomeCost() or something to figure out when to return, its being simulated by a simple counter for now
-            // decision.put("action", "stop"); 
-            command.stop();
+        if (counter % 2 == 0){
+            drone.setNextMove("echo");
+            command.echo(EAST);
+            // if (response.getFound() == "OUT_OF_RANGE"){
+                // needs a way to turn around
+                command.stop();
+            // }
         } else{
-            if (counter % 2 == 0){
-                //JSONObject parameters = new JSONObject();
-                // temporarily set direction as E, need to determine the correct echo direction later
-                //parameters.put("direction", "E");
-                //decision.put("action", ECHO);
-                //decision.put("parameters", parameters);
-                command.echo(EAST); 
-                lastCommand = "echo";
-            }
-            else {
-                lastCommand = "fly";
-                command.fly();
-            }
+            drone.setNextMove("fly");
+            command.fly();
         }
         counter += 1;
         // add a switch case for the command object and depending on what its holding grab a response
--- src/main/java/ca/mcmaster/se2aa4/island/team101/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team101/Drone.java
@@ -10,17 +10,24 @@ public class Drone extends Traveler {
     private AirDecision nextMove;
     private Compass compass;
     private AreaMap map;
+    private String lastCommand; 
 
     public Drone(JSONInitialization initializer){
         this.initializer = initializer;
         this.charge = initializer.getBatteryLevel();
         this.nextMove = new AirDecision(this); // this is weird idk if there's another way
         this.compass = new Compass(initializer.getDirection());
+        this.lastCommand = "echo";
     }
 
     @Override
-    public void setNextMove(){
-        nextMoveStr = nextMove.decide();
+    public void setNextMove(String command){
+        lastCommand = command;
+    }
+
+    @Override
+    public String getLastMove(){
+        return lastCommand;
     }
 
     // should sweep and update everything like heading battery etc
@@ -29,10 +36,18 @@ public class Drone extends Traveler {
     public void update(Response response){ 
         setCharge(response.getCost());
         // only update the map if it was a scanresponse
-        if (response instanceof ScanResponse) {
-            // the Point coordinate of the drone is held and maintained in the compass. 
-            // it grabs it in here to update the map
-            map.updateMap(compass.getPosition(), (ScanResponse)response);
+        switch (lastCommand) {
+            case "scan":
+                // the Point coordinate of the drone is held and maintained in the compass. 
+                // it grabs it in here to update the map
+                map.updateMap(compass.getPosition(), (ScanResponse)response);
+                break;
+            case "fly":
+                compass.advance();
+            case "echo":
+                response = (EchoResponse)response;
+            default:
+                break;
         }
         // need to update the compass somehow 
     }
