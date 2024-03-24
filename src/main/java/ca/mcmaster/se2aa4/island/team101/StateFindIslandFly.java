f6ac9f2a69180ceeb4d7565f01e02bcb2be18d49
git@github.com <savvy.liu@hotmail.com>
2024-03-24 01:31:28 -0400

    fixes to the state machine, still doesnt work

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team101/StateFindIslandFly.java
@@ -0,0 +1,53 @@
+// ECHO FORWARD <-- INITIAL STATE
+// this never uses the context because it doesn't matter; regardless of the context this first state needs
+// to just echo forward
+
+package ca.mcmaster.se2aa4.island.team101;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StateFindIslandFly extends State{
+    
+    private final Logger logger = LogManager.getLogger();
+    private EchoResponse latestEcho;
+    private String found;
+    private int range;
+
+    public StateFindIslandFly(Drone drone, DroneContext context){ 
+        super(drone, context); 
+        latestEcho = (EchoResponse) latestResponse;
+        found = latestEcho.getFound();
+        range = latestEcho.getRange();
+    }
+
+    @Override
+    public String getNextMove(){
+
+
+        if (context.left){
+            logger.info("********************** LAST ECHO WAS LEFT ECHO");
+            context.left = false;
+            command.echo(compass.getRight());
+            return command.toString();
+        }
+
+        
+        if (!context.left){
+            logger.info("********************** LAST ECHO WAS RIGHT ECHO");
+            context.left = true;
+            command.echo(compass.getLeft());
+            return command.toString();
+        }
+
+    
+        return command.toString();
+
+    } 
+
+    @Override
+    public String getNextState(){
+        return "StateFindIslandEcho";
+    }
+
+}
